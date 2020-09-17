45689a3f589b9843af6a741008b8dd3510f4152a
ngoldin <nikola.gnv@gmail.com>
2020-09-17 14:24:20 +0300

    remove keys

--- app/src/main/java/ru/rosbank/mbdg/myapplication/Constants.kt
+++ app/src/main/java/ru/rosbank/mbdg/myapplication/Constants.kt
@@ -1,8 +1,5 @@
 package ru.rosbank.mbdg.myapplication
 
 object Constants {
-
-    const val API_KEY = "app_oBcXz2z9j8spKPL2T7sZwQaQN5Jzme"  //my api key
-    //    const val API_KEY = "app_oghnMxjFfAuKogzaGYkBSGJFd6tmwH"    //test api key
-    const val APPSFLYER_DEV_KEY = "AbwD69b6Foj9WT3huU9emh"
+    const val API_KEY = "YOUR_API_KEY"
 }
