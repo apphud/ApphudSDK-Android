9fc6b42753afcdbb60fa5232f4c50e51524d8183
ren6 <ren6@apphud.com>
2021-01-25 15:22:58 +0300

    update App

--- app/src/main/java/com/apphud/app/Constants.kt
+++ app/src/main/java/com/apphud/app/Constants.kt
@@ -1,8 +1,5 @@
 package com.apphud.app
 
 object Constants {
-
-    const val API_KEY = "app_oBcXz2z9j8spKPL2T7sZwQaQN5Jzme"  //my api key
-    //    const val API_KEY = "app_oghnMxjFfAuKogzaGYkBSGJFd6tmwH"    //test api key
-    const val APPSFLYER_DEV_KEY = "AbwD69b6Foj9WT3huU9emh"
+    const val API_KEY = "app_EzgZvMy2qRcSsU2uRuDxd9GaqU8BCV"
 }
