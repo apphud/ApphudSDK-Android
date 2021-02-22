036c191f7e5268e800b026c4a6c0d76df63bd94b
Aleksey Ivantsov <a.ivantsov@apphud.com>
2021-02-22 20:39:59 +0300

    APH-401 - Experimental fixes to improve SkuDetailsWrapper async

--- sdk/src/main/java/com/apphud/sdk/internal/SkuDetailsWrapper.kt
+++ sdk/src/main/java/com/apphud/sdk/internal/SkuDetailsWrapper.kt
@@ -9,7 +9,7 @@ import com.apphud.sdk.ProductId
 import com.apphud.sdk.domain.PurchaseRecordDetails
 import com.apphud.sdk.isSuccess
 import com.apphud.sdk.logMessage
-import java.io.Closeable
+import kotlin.concurrent.thread
 
 typealias SkuType = String
 typealias ApphudSkuDetailsCallback = (List<SkuDetails>) -> Unit
@@ -17,48 +17,63 @@ typealias ApphudSkuDetailsRestoreCallback = (List<PurchaseRecordDetails>) -> Uni
 
 internal class SkuDetailsWrapper(
     private val billing: BillingClient
-) : Closeable {
-
+) : BaseAsyncWrapper() {
     var callback: ApphudSkuDetailsCallback? = null
     var restoreCallback: ApphudSkuDetailsRestoreCallback? = null
 
     fun restoreAsync(@BillingClient.SkuType type: SkuType, records: List<PurchaseHistoryRecord>) {
-
         val products = records.map { it.sku }
         val params = SkuDetailsParams.newBuilder()
             .setSkusList(products)
             .setType(type)
             .build()
-        billing.querySkuDetailsAsync(params) { result, details ->
-            when (result.isSuccess()) {
-                true -> {
-                    val values = details ?: emptyList()
-                    val purchases = values.map { detail ->
-                        PurchaseRecordDetails(
-                            record = records.first { it.sku == detail.sku },
-                            details = detail
-                        )
+
+        thread(start = true, name = "restoreAsync+$type") {
+            while (!billing.isReady) {
+                ApphudLog.log("restoreAsync is on waiting for ${retryDelay}ms for $type")
+                Thread.sleep(retryDelay)
+                if(retryCount++>=retryCapacity)
+                    break
+            }
+            billing.querySkuDetailsAsync(params) { result, details ->
+                when (result.isSuccess()) {
+                    true -> {
+                        val values = details ?: emptyList()
+                        val purchases = values.map { detail ->
+                            PurchaseRecordDetails(
+                                record = records.first { it.sku == detail.sku },
+                                details = detail
+                            )
+                        }
+                        when (purchases.isEmpty()) {
+                            true -> ApphudLog.log("SkuDetails return empty list for $type and records: $records")
+                            else -> restoreCallback?.invoke(purchases)
+                        }
                     }
-                    when (purchases.isEmpty()) {
-                        true -> ApphudLog.log("SkuDetails return empty list for $type and records: $records")
-                        else -> restoreCallback?.invoke(purchases)
-                    }
+                    else -> result.logMessage("restoreAsync type: $type products: $products")
                 }
-                else -> result.logMessage("restoreAsync type: $type products: $products")
             }
         }
     }
 
     fun queryAsync(@BillingClient.SkuType type: SkuType, products: List<ProductId>) {
-
         val params = SkuDetailsParams.newBuilder()
             .setSkusList(products)
             .setType(type)
             .build()
-        billing.querySkuDetailsAsync(params) { result, details ->
-            when (result.isSuccess()) {
-                true -> callback?.invoke(details ?: emptyList())
-                else -> result.logMessage("queryAsync type: $type products: $products")
+
+        thread(start = true, name = "queryAsync+$type") {
+            while (!billing.isReady) {
+                ApphudLog.log("queryAsync is on waiting for ${retryDelay}ms for $type")
+                Thread.sleep(retryDelay)
+                if(retryCount++>=retryCapacity)
+                    break
+            }
+            billing.querySkuDetailsAsync(params) { result, details ->
+                when (result.isSuccess()) {
+                    true -> callback?.invoke(details ?: emptyList())
+                    else -> result.logMessage("queryAsync type: $type products: $products")
+                }
             }
         }
     }
