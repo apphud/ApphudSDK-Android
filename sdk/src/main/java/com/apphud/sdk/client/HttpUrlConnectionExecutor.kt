69071ea1faa35a992ed9d9d3134bb5550e010a2a
Vladimir Slatvinski <v.slatvinski@apphud.com>
2021-10-27 10:09:56 +0300

    upload sdk to maven central

--- sdk/src/main/java/com/apphud/sdk/client/HttpUrlConnectionExecutor.kt
+++ sdk/src/main/java/com/apphud/sdk/client/HttpUrlConnectionExecutor.kt
@@ -1,21 +1,16 @@
 package com.apphud.sdk.client
 
-import android.view.View
 import com.apphud.sdk.ApphudLog
-import com.apphud.sdk.ApphudUtils
 import com.apphud.sdk.BuildConfig
 import com.apphud.sdk.client.dto.AttributionDto
 import com.apphud.sdk.client.dto.DataDto
 import com.apphud.sdk.client.dto.ResponseDto
 import com.apphud.sdk.isSuccess
 import com.apphud.sdk.parser.Parser
-import com.google.gson.reflect.TypeToken
 import java.io.BufferedReader
 import java.io.InputStream
 import java.io.InputStreamReader
-import java.net.SocketTimeoutException
 import java.net.URL
-import java.net.UnknownHostException
 import javax.net.ssl.HttpsURLConnection
 
 class HttpUrlConnectionExecutor(
