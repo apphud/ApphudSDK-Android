44ff56455e151eb28f8a8484f83df8bef30b87a8
Vladimir Slatvinski <v.slatvinski@apphud.com>
2021-12-17 17:34:22 +0300

    AHS-106 Добавить параметр observer_mode = true|false вместе с чеком

--- sdk/src/main/java/com/apphud/sdk/ApphudInternal.kt
+++ sdk/src/main/java/com/apphud/sdk/ApphudInternal.kt
@@ -979,7 +979,8 @@ internal object ApphudInternal {
                     price_amount_micros = details?.priceAmountMicros,
                     subscription_period = details?.subscriptionPeriod,
                     paywall_id = paywall_id,
-                    product_bundle_id = apphud_product_id
+                    product_bundle_id = apphud_product_id,
+                    observer_mode = false
                 )
             )
         )
@@ -996,7 +997,8 @@ internal object ApphudInternal {
                     price_amount_micros = purchase.details.priceAmountMicros,
                     subscription_period = purchase.details.subscriptionPeriod,
                     paywall_id = null,
-                    product_bundle_id = null
+                    product_bundle_id = null,
+                    observer_mode = true
                 )
             }
         )
