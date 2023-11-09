ddfcdf3c6e6e9bd0e0d0a8e10d397e533c81f653
ren6 <ren6@apphud.com>
2023-11-09 13:51:00 +0700

    add userCanceled function

--- demo/src/main/java/com/apphud/demo/ui/products/ProductsFragment.kt
+++ demo/src/main/java/com/apphud/demo/ui/products/ProductsFragment.kt
@@ -46,7 +46,7 @@ class ProductsFragment : Fragment() {
                             fragment.offerSelected = { offer ->
                                 Apphud.purchase(activity, product, offer.offerToken){ result ->
                                     result.error?.let{ err->
-                                        Toast.makeText(activity, err.message, Toast.LENGTH_SHORT).show()
+                                        Toast.makeText(activity, if (result.userCanceled()) "User Canceled" else err.message, Toast.LENGTH_SHORT).show()
                                     }?: run{
                                         Toast.makeText(activity, R.string.success, Toast.LENGTH_SHORT).show()
                                     }
@@ -60,7 +60,7 @@ class ProductsFragment : Fragment() {
                         if(product.product_id == "com.apphud.demo.nonconsumable.premium"){
                             Apphud.purchase(activity = activity, apphudProduct = product, consumableInappProduct = false){ result ->
                                 result.error?.let{ err->
-                                    Toast.makeText(activity, err.message, Toast.LENGTH_SHORT).show()
+                                    Toast.makeText(activity, if (result.userCanceled()) "User Canceled" else err.message, Toast.LENGTH_SHORT).show()
                                 }?: run{
                                     Toast.makeText(activity, R.string.success, Toast.LENGTH_SHORT).show()
                                 }
@@ -68,7 +68,7 @@ class ProductsFragment : Fragment() {
                         } else {
                             Apphud.purchase(activity = activity, apphudProduct = product, consumableInappProduct = true){ result ->
                                 result.error?.let{ err->
-                                    Toast.makeText(activity, err.message, Toast.LENGTH_SHORT).show()
+                                    Toast.makeText(activity, if (result.userCanceled()) "User Canceled" else err.message, Toast.LENGTH_SHORT).show()
                                 }?: run{
                                     Toast.makeText(activity, R.string.success, Toast.LENGTH_SHORT).show()
                                 }
