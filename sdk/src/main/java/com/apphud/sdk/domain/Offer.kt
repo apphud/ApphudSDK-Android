e6795c823970f6988a949b114aa1531bbb4cb08e
Vladimir Slatvinski <v.slatvinski@apphud.com>
2024-03-28 08:26:06 +0300

    minor fix

--- sdk/src/main/java/com/apphud/sdk/domain/Offer.kt
+++ sdk/src/main/java/com/apphud/sdk/domain/Offer.kt
@@ -3,9 +3,12 @@ package com.apphud.sdk.domain
 import com.xiaomi.billingclient.api.SkuDetails
 
 class Offer(skuDetails: SkuDetails, offerTokenId: String) {
-    val base_plan_id: String? = skuDetails.subscriptionOfferDetails?.find { it.offerToken == offerTokenId }?.basePlanId
-    val offer_id: String? = skuDetails.subscriptionOfferDetails?.find { it.offerToken == offerTokenId }?.offerId
+    val base_plan_id: String? =
+        skuDetails.subscriptionOfferDetails?.find { it.offerToken == offerTokenId }?.basePlanId
+    val offer_id: String? =
+        skuDetails.subscriptionOfferDetails?.find { it.offerToken == offerTokenId }?.offerId
     val pricing_phases: List<PriceInfo>? =
         skuDetails.subscriptionOfferDetails?.find {
             it.offerToken == offerTokenId
         }?.pricingPhases?.pricingPhaseList?.map { PriceInfo(it) }
+}
