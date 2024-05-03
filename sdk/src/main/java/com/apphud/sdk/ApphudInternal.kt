e04218ffa39a951598ede03e40638304a5ea1af1
Vladimir Slatvinski <v.slatvinski@apphud.com>
2024-05-03 11:35:54 +0300

    minor changes

--- sdk/src/main/java/com/apphud/sdk/ApphudInternal.kt
+++ sdk/src/main/java/com/apphud/sdk/ApphudInternal.kt
@@ -569,8 +569,8 @@ internal object ApphudInternal {
     }
 
     internal fun performWhenOfferingsPrepared(preferredTimeout: Double?, callback: (ApphudError?) -> Unit) {
-        if (preferredTimeout != null) {
-            this.preferredTimeout = max(preferredTimeout!!, APPHUD_DEFAULT_MAX_TIMEOUT)
+        preferredTimeout?.let{
+            this.preferredTimeout = max(it, APPHUD_DEFAULT_MAX_TIMEOUT)
             this.offeringsCalledAt = System.currentTimeMillis()
             currentPoductsLoadingCounts = 0
         }
